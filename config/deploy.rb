aaf7d9f1cfb526f353d6c2468fe1b22d395fc4b1
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2016-09-08 18:46:32 -0300

    add env to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :scm                     , :git
 set :repo_url                , 'git@github.com:DesafioLatam/EjemploCapistrano2.git'
 set :keep_releases           , 5
 set :ssh_options             , { forward_agent: true, user: fetch(:user) , keys: %w(~/.ssh/id_rsa.pub) }
+set :rails_env               , :production
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/secrets.yml .env}
